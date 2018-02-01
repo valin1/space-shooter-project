31b91729fcd2a84b47d82acf02be354927b67318
Vanessa Lin <valin@berkeley.edu>
2018-01-31 21:24:03 -0800

    First commit

---
+++ SpaceShooterProject/Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour {
+	private Rigidbody rb;
+	public float tumble;
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+		
+	}
+}
